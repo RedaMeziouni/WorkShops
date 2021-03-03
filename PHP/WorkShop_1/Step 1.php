d70fc097c9adb65f1c73b9dd5a8ac5eb268c86c2
RedaMeziouni <meziounidb@gmail.com>
2021-03-03 06:42:08 +0100

    PHP Workshop

---
+++ PHP/WorkShop_1/Step 1.php
@@ -0,0 +1,10 @@
+<?php
+// Chaine de caractéres 
+$string = "YouCode Campus Youssoufia";
+echo $string;
+
+// Varable contenant un entier
+$int = 46;
+echo $int;
+
+// 
---
+++ PHP/WorkShop_2/Step 1.php
@@ -0,0 +1,17 @@
+<!-- First -->
+<?php
+$i = 0;
+while ($i <= 10) {
+    echo "$i" . "<br>";
+    $i++;
+}
+?>
+
+<!-- Second -->
+<?php 
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . "<br>";
+}
+?>
+
+<!-- Third -->
