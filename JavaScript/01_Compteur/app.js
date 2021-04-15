0eb7acaa55f358b383b6b69f37ead93b87edbfa3
RedaMeziouni <meziounidb@gmail.com>
2021-04-15 12:28:57 +0000

    2nd solution

--- JavaScript/01_Compteur/app.js
+++ JavaScript/01_Compteur/app.js
@@ -29,3 +29,39 @@ btns.forEach(function (btn) {
     value.textContent = count;
   });
 });
+
+// *************2nd Solution*************
+
+// let add = document.querySelector('.increase'),
+//     sub = document.querySelector('.decrease'),
+//     reset = document.querySelector('.reset'),
+//     conter = document.querySelector('#value')
+
+// x = 0
+// function style(x) {
+//     if (x > 0) {
+//         value.style.color = "green"
+//     }
+//     if (x == 0) {
+//         value.style.color = "#222"
+//     }
+//     if (x < 0) {
+//         value.style.color = "red"
+//     }
+// }
+// add.addEventListener('click', () => {
+//     conter.innerHTML = ++x
+//     style(x)
+
+// });
+// sub.addEventListener('click', () => {
+//     conter.innerHTML = --x
+//     style(x)
+
+// });
+// reset.addEventListener('click', () => {
+//     x = 0
+//     conter.innerHTML = x
+//     style(x)
+
+// })
